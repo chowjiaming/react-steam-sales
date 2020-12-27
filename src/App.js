bf8db0b2d87d884080fb6539192a08f51506658c
joefstack <joefstack@gmail.com>
2020-12-27 16:33:42 -0700

    added search form

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import { useState } from "react";
 import { Container, Spinner } from "react-bootstrap";
 import useFetchGames from "./helpers/useFetchGames";
 import Game from "./components/Game";
+import SearchForm from "./components/SearchForm";
 
 function App() {
   const [params, setParams] = useState({});
   const [page, setPage] = useState(0);
   const { games, loading, error, hasNextPage } = useFetchGames(params, page);
+
+  const handleParamChange = (e) => {
+    const param = e.target.name;
+    const value = e.target.value;
+    setPage(0);
+    setParams((prevParams) => {
+      return { ...prevParams, [param]: value };
+    });
+  };
 
   const handleError = (error) => {
     if (error.message.includes("429")) {
@@ -20,6 +30,7 @@ function App() {
     <Container>
       <h1>Seach Steam Sales</h1>
       <h5>Powered by CheapShark API</h5>
+      <SearchForm params={params} onParamChange={handleParamChange} />
       {loading && <Spinner animation="border" variant="primary" />}
       {error && <h1>{handleError(error)}</h1>}
       {games.map((game) => {
---
+++ src/components/SearchForm.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Form, Col } from "react-bootstrap";
+
+export default function SearchForm({ params, onParamChange }) {
+  return (
+    <Form className="mb-4">
+      <Form.Row className="align-items-end">
+        <Form.Group as={Col}>
+          <Form.Label>Title</Form.Label>
+          <Form.Control
+            placeholder="eg. Bioshock"
+            onChange={onParamChange}
+            value={params.title}
+            name="title"
+            type="text"
+          />
+        </Form.Group>
+        <Form.Group as={Col}>
+          <Form.Label>Highest Price</Form.Label>
+          <Form.Control
+            placeholder="eg. 29.99"
+            onChange={onParamChange}
+            value={params.upperPrice}
+            name="upperPrice"
+            type="text"
+          />
+        </Form.Group>
+        <Form.Group as={Col}>
+          <Form.Label>Lowest Price</Form.Label>
+          <Form.Control
+            placeholder="eg. 49.99"
+            onChange={onParamChange}
+            value={params.lowerPrice}
+            name="lowerPrice"
+            type="text"
+          />
+        </Form.Group>
+      </Form.Row>
+    </Form>
+  );
+}
